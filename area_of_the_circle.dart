0e6fed3104e94ad8af1d48b3f6c604628b2066b2
Chijith_Jerin.S <chijithjerin2004@gmail.com>
2023-04-27 21:43:43 +0530

    Initial commit

---
+++ area_of_the_circle.dart
@@ -0,0 +1,6 @@
+void main() {
+  var radius = 25;
+  double pie = 3.14;
+  var area = pie * radius * radius;
+  print(area);
+}
---
+++ datatypes.dart
@@ -0,0 +1,6 @@
+void main() {
+  var num1 = 123440;
+  var num2 = 2123;
+  var value = num1 ~/ num2; // used to find quotient
+  print("The Answer is: ${value}");
+}
