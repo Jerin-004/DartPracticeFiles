3d49721f2410c96a0652af84060ac189b08174fa
Chijith_Jerin.S <chijithjerin2004@gmail.com>
2023-05-04 20:40:50 +0530

    Create iteration.dart

---
+++ iteration.dart
@@ -0,0 +1,15 @@
+void main() {
+  
+  List<String> football_players = [
+    "Lionel Messi",
+    "Ronaldo",
+    "Neymar",
+    "Mbappe",
+    "Hulk"
+  ];
+
+  for (var i = 0; i < football_players.length; i++) {
+    print(football_players[i]);
+  }
+
+}
