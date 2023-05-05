fd132744ed0a79a182f56b0bf3681f92a65ad658
Chijith_Jerin.S <chijithjerin2004@gmail.com>
2023-05-05 20:24:53 +0530

    Update datatypes.dart

--- datatypes.dart
+++ datatypes.dart
@@ -1,6 +1,6 @@
 void main() {
   var num1 = 123440;
   var num2 = 2123;
-  var value = num1 ~/ num2; // used to find quotient
+  var value = num1 ~/ num2; // used to find quotient the symbol name is tesla
   print("The Answer is: ${value}");
 }
