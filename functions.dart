aad9afcae2263bc070441c2854961aa9009c2577
Chijith_Jerin.S <chijithjerin2004@gmail.com>
2023-05-08 20:39:17 +0530

    simple dynamic functions

---
+++ functions.dart
@@ -0,0 +1,34 @@
+dynamic add(num1,num2){
+  
+  return num1 + num2;
+
+}
+
+dynamic subtract(num1,num2){
+  
+  return num1 - num2;
+
+}
+
+dynamic multiplication(num1,num2){
+  
+  return num1 * num2;
+
+}
+
+dynamic divisioin(num1,num2){
+  
+  return num1 / num2;
+
+}
+
+void main(){
+
+  var num1 = 10.87;
+  var num2 = 90.43;
+  print(add(num1, num2));
+  print(subtract(num1, num2));
+  print(multiplication(num1, num2));
+  print(divisioin(num1, num2));
+
+}
